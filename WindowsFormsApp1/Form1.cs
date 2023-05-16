fa7b9d3796b86b66dfe263a515e57861fd23954a
Администратор <@DESKTOP-QGQ2PB4>
2023-05-16 19:15:44 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using WindowsFormsApp1;
+
+namespace WindowsFormsApp1
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("xmltext.xml");
+            foreach (XmlNode node in doc.DocumentElement)
+            {
+                string name = node.Attributes[0].Value;
+                int age = int.Parse(node["Age"].InnerText);
+                bool programmer = bool.Parse(node["Programmer"].InnerText);
+                listBox1.Items.Add(new Employee(name, age, programmer));
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+            {
+                propertyGrid1.SelectedObject = listBox1.SelectedItem;
+            }
+        }
+    }
+}
